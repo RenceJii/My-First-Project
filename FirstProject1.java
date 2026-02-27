b583e1e2df829a19d94cd73a9d3ea7f6861e9bf6
RenceJii <espejonclarence39@gmail.com>
2026-02-27 22:24:59 +0800

    This is my first change

--- FirstProject1.java
+++ FirstProject1.java
@@ -1,5 +1,6 @@
 class FirstProject {    
     public static void main(String[] args) {
     System.out.println("This is my first Java project!");
+    System.out.println("I am learning Java programming.");
   }
 }
